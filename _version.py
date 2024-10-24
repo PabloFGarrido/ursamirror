69fd1df528a93f8e5ba67b038e2a62ead4f1a531
Pablo F. Garrido <f.garrido.pablo@gmail.com>
2024-10-24 15:05:30 +0200

    First Release for Pypi

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev16+g53d1e3f.d20241024'
-__version_tuple__ = version_tuple = (0, 1, 'dev16', 'g53d1e3f.d20241024')
+__version__ = version = '0.2.1.dev0+g4642ede.d20241024'
+__version_tuple__ = version_tuple = (0, 2, 1, 'dev0', 'g4642ede.d20241024')
