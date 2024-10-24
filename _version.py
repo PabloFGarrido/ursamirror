4642ede5f9d41083b577333a873328a4409a55fe
Pablo F. Garrido <f.garrido.pablo@gmail.com>
2024-10-24 14:45:43 +0200

    added wheel and source distribution files

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev7+g992beaf.d20241023'
-__version_tuple__ = version_tuple = (0, 1, 'dev7', 'g992beaf.d20241023')
+__version__ = version = '0.1.dev16+g53d1e3f.d20241024'
+__version_tuple__ = version_tuple = (0, 1, 'dev16', 'g53d1e3f.d20241024')
